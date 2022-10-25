996b6ba8d4cb99a0e6cca51bac52999be1088640
Sebastian Paysse <sebastianpaysse@gmail.com>
2022-10-25 15:46:31 -0300

    make it better

--- src/database.js
+++ src/database.js
@@ -2,33 +2,34 @@ const mongoose = require('mongoose');
 
 // mongoose.set('useFindAndModify', false);
 
-mongoose.connect('mongodb://localhost/tbs',{
-//         useCreateIndex:true,
-//         useNewUrlParser:true,
-//         useUnifiedTopology:true,
-// //        useFindAndModify:false
-})
+// mongoose.connect('mongodb://localhost/tbs',{
+// //         useCreateIndex:true,
+// //         useNewUrlParser:true,
+// //         useUnifiedTopology:true,
+// // //        useFindAndModify:false
+// })
 
-.then(db => console.log('DB esta correctamente conectada'))
-.catch(err => console.error(err));
+// .then(db => console.log('DB esta correctamente conectada'))
+// .catch(err => console.error(err));
 
 
 //Conexion al ambiente de produccion 
 
-// const dbConnecton = async()=>{
-// try {
-//     await mongoose.connect('mongodb+srv://sebatbsit:KcZorfwjhcurNKEI@tbsit.12drd1s.mongodb.net/testtbsit', {
-//         // useCreateIndex:true,
-//         useNewUrlParser:true,
-//         useUnifiedTopology:true,
-//         // useFindAndModify:false
-//     })
-//         .then(db => console.log(`DB is connected`))
-//         .catch(err => console.error(err));
+const dbConnecton = async()=>{
+try {
+    await mongoose.connect('mongodb+srv://sebatbsit:KcZorfwjhcurNKEI@tbsit.12drd1s.mongodb.net/testtbsit', {
+        // useCreateIndex:true,
+        useNewUrlParser:true,
+        useUnifiedTopology:true,
+        // useFindAndModify:false
+    })
+        .then(db => console.log(`DB is connected`))
+        .catch(err => console.error(err));
         
-// } catch (error) {
-//     console.log('No se conecto la BD', error);
-//     throw new Error('Error en la conexion de la base de datos');
-// };
-// };
-// dbConnecton()
+} catch (error) {
+    console.log('No se conecto la BD', error);
+    throw new Error('Error en la conexion de la base de datos');
+};
+};
+
+dbConnecton()
