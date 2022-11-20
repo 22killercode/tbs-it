32e0525b64521b45b1e3c7270f4f84501ac7969f
Sebastian Paysse <sebastianpaysse@gmail.com>
2022-11-19 21:46:06 -0300

    make it better

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-// mongoose.set('useFindAndModify', false);
+//mongoose.set('useFindAndModify', false);
 
 // mongoose.connect('mongodb://localhost/tbs',{
 // //         useCreateIndex:true,
