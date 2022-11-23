9f584a7f614192ca039da229b66176583f4b6b61
Sebastian Paysse <sebastianpaysse@gmail.com>
2022-11-23 14:13:52 -0300

    make it better

--- src/database.js
+++ src/database.js
@@ -4,21 +4,7 @@ const mongoose = require('mongoose');
 
 
 try {
-        mongoose.connect('mongodb://localhost/tbs',{
-        //         useCreateIndex:true,
-        //         useNewUrlParser:true,
-        //         useUnifiedTopology:true,
-        // //        useFindAndModify:false
-        })
-
-        .then(db => console.log('DB esta correctamente conectada'))
-        .catch(err => console.error(err));
-
-    } catch (error) {
-
-
-    // Conexion al ambiente de produccion 
-
+        
     const dbConnecton = async()=>{
         try {
             await mongoose.connect('mongodb+srv://sebatbsit:KcZorfwjhcurNKEI@tbsit.12drd1s.mongodb.net/testtbsit', {
@@ -37,6 +23,21 @@ try {
         };
         
         dbConnecton()
+
+    } catch (error) {
+
+        mongoose.connect('mongodb://localhost/tbs',{
+        //         useCreateIndex:true,
+        //         useNewUrlParser:true,
+        //         useUnifiedTopology:true,
+        // //        useFindAndModify:false
+        })
+
+        .then(db => console.log('DB esta correctamente conectada'))
+        .catch(err => console.error(err));
+
+
+
 
 }
 
