8e14c3a16136819afb516672b5930912d75a0d7c
Sebastian Paysse <sebastianpaysse@gmail.com>
2022-12-12 13:45:57 -0300

    make it better

--- src/routes/indexRoutes.js
+++ src/routes/indexRoutes.js
@@ -3638,10 +3638,10 @@ router.post('/contacto',async (req, res) => {
     // envia mail
 const cotiEntro = `<html>
 <body style="padding:1rem; margin:auto; background:whitesmoke; height:auto; box-shadow:0.2rem 0.4rem 0.7rem 0.7rem black; width:80%; border-radius:1.5rem; color:black; border-top:lightgray 0.2rem solid; border-left:lightgray 0.2rem solid; border-bottom:black 0.4rem solid; border-right:black 0.4rem solid; "font-family:'Times New Roman', Times, serif; white-space:pre-line; word-break:break-all;>
-<div style="diplay:grid; place-items:center">
+<div style="justify-content: center ; align-items: center;">
 
-<img style=width:90%; height:50%; border-radius:1.5rem; border-top:lightgray 0.2rem solid; border-left:lightgray 0.2rem solid; border-bottom:lightgray 0.4rem solid; border-right:lightgray 0.4rem solid;  "src="https://tbs-it.net/images/tbsLogo3.jpg">
-
+<img style="z-index:99999999999999999999999999999 !important; width:90%; height:50%; diplay:grid; place-items:center;  border-radius:1.5rem; border-top:lightgray 0.2rem solid; border-left:lightgray 0.2rem solid; border-bottom:black 0.4rem solid; border-right:black 0.4rem solid;
+"src="https://tbs-it.net/images/tbsLogo3.jpg">
 <br>
 <br>
     <p>
