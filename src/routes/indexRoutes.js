0db122668fcc8b0d4ba3355e73ece21f367e0965
Sebastian Paysse <sebastianpaysse@gmail.com>
2022-12-15 19:45:11 -0300

    make it better

--- src/routes/indexRoutes.js
+++ src/routes/indexRoutes.js
@@ -2319,7 +2319,7 @@ const contentHTML = `<html>
 <body style="padding:1rem; margin:auto; background:whitesmoke; height:auto; box-shadow:0.2rem 0.4rem 0.7rem 0.7rem black; width:80%; border-radius:1.5rem; color:black; border-top:lightgray 0.2rem solid; border-left:lightgray 0.2rem solid; border-bottom:black 0.4rem solid; border-right:black 0.4rem solid; "font-family:'Times New Roman', Times, serif; white-space:pre-line; word-break:break-all;>
 
 <a href="https://tbs-it.net/">
-    <img style="margin-left:1rem;z-index:99999999999999999999999999999 !important; width:40%; height:7%; diplay:grid; place-items:center; border-radius:1.5rem; border-top:lightgray 0.2rem solid; border-left:lightgray 0.2rem solid; border-bottom:lightgray 0.4rem solid; border-right:lightgray 0.4rem solid;
+    <img style="margin:auto;z-index:99999999999999999999999 !important; width:40%; height:7%; diplay:grid; place-items:center; border-radius:1.5rem; border-top:lightgray 0.2rem solid; border-left:lightgray 0.2rem solid; border-bottom:lightgray 0.4rem solid; border-right:lightgray 0.4rem solid;
     "src="https://tbs-it.net/images/tbsLogo3.jpg"/>
 </a>
 <br>
