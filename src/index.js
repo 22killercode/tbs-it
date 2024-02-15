07385d61a239a526a905f061d10f9e710a4cb982
Sebastian Paysse <sebastianpaysse@gmail.com>
2024-02-15 09:55:37 -0300

    adptando blog

--- src/index.js
+++ src/index.js
@@ -100,10 +100,11 @@ app.use(express.static(path.join(__dirname, 'views')));
 app.use(express.static(path.join(__dirname, 'layouts')));
 app.use(express.static(path.join(__dirname, 'partials')));
 app.use(express.static(path.join(__dirname, 'uploads')));
+app.use('/uploads', express.static('uploads'));
 // Configura una ruta estática para servir imágenes desde la carpeta 'uploads' para los blogs
 //app.use('/uploads', express.static('C:\\Users\\Coderian\\Desktop\\pruebaTBS\\src\\uploads'));
 //app.use('/uploads', express.static('pruebaTBS\\src\\uploads'));
-app.use('/uploads', express.static('https://tbs-it.net/uploads'));
+
 
 // app 4004
 // app.use((req, res, next) => {
