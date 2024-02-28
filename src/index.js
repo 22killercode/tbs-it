0fbac83952234f6121a843e41957da31662b3233
Sebastian Paysse <sebastianpaysse@gmail.com>
2024-02-28 11:45:11 -0300

    ultimo deuis

--- src/index.js
+++ src/index.js
@@ -45,6 +45,13 @@ app.use(cors({
     credentials: true,
 }));
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5500/index.html'); // Reemplaza con el dominio de tu aplicación
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
